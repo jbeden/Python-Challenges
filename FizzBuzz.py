1eb12596e5f2b5074d53f5dafdab6ad8e3c96a2b
jbeden <johnbedenbaugh9@gmail.com>
2020-02-23 11:45:42 -0500

    Create FizzBuzz.py

---
+++ FizzBuzz.py
@@ -0,0 +1,12 @@
+
+
+for i in range(100):
+  x = i + 1
+  if(x % 3 == 0 or x % 5 == 0):
+    if(x % 3 == 0):
+      print("Fizz", end="")
+    if(x % 5 == 0):
+      print("Buzz", end="")
+  else:
+    print(x, end="")
+  print()
